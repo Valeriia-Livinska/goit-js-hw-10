dab19ccf1e49621e5fd64dc3ee74074df7a7e346
Valeriia-Livinska <valerylivi@gmail.com>
2022-09-22 01:36:28 +0300

    fixed error

--- src/index.js
+++ src/index.js
@@ -13,19 +13,31 @@ input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 function onInput(event) {
   const countryName = event.target.value.trim();
 
-  fetchCountries(countryName).then(data => {
+  if (!countryName) {
     clearMarkup();
+    return;
+  }
 
-    if (data.length > 10) {
-      Notify.info('Too many matches found. Please enter a more specific name.');
-    }
-    if (data.length > 2 && data.length <= 10) {
-      countriesListMarkup(data);
-    }
-    if (data.length === 1) {
-      oneCountryMarkup(data);
-    }
-  });
+  fetchCountries(countryName)
+    .then(data => {
+      clearMarkup();
+
+      if (data.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      }
+      if (data.length > 2 && data.length <= 10) {
+        countriesListMarkup(data);
+      }
+      if (data.length === 1) {
+        oneCountryMarkup(data);
+      }
+    })
+    .catch(error => {
+      clearMarkup();
+      Notify.failure('Oops, there is no country with that name');
+    });
 }
 
 function countriesListMarkup(countries = []) {
